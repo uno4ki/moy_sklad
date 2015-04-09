ac0e9bbe233a546a9abb4d0f445e448057570af8
uno4ki <spam@junki.org>
2015-04-09 18:38:24 +0300

    Code cleanup EmptyCollection exception added Version bump

--- lib/moy_sklad/client/collection.rb
+++ lib/moy_sklad/client/collection.rb
@@ -5,40 +5,13 @@ require 'active_support/core_ext/hash/conversions'
 require 'active_support/core_ext/hash/indifferent_access'
 
 module MoySklad::Client
-  if ActiveResource::VERSION::STRING < '4.0.0'
-    class Collection
-      include Enumerable
-
-      delegate :collect, :each, :length, :to => :to_a
-
-      attr_accessor :elements
-
-      def to_a
-        @elements
-      end
-
-      def collect!
-        return @elements unless block_given?
-        set = []
-        each { |o| set << yield(o) }
-        @elements = set
-        self
-      end
-
-      alias map! collect!
-    end
-  else
-    class Collection < ActiveResource::Collection
-    end
-  end
-
-  class Collection
+  class Collection < ActiveResource::Collection
     attr_reader :metadata
 
     def initialize(data)
-      fail MoySklad::BadApiResponseError unless data
-      fail MoySklad::BadApiResponseError unless data[:data]
-      fail MoySklad::BadApiResponseError unless data[:data]['collection']
+      fail MoySklad::BadApiResponse unless data
+      fail MoySklad::BadApiResponse unless data[:data]
+      fail MoySklad::EmptyCollection unless data[:data]['collection']
 
       @elements = data[:data]['collection'].delete(data[:object])
       @elements = [@elements] if @elements.is_a?(Hash)
--- lib/moy_sklad/error.rb
+++ lib/moy_sklad/error.rb
@@ -1,4 +1,5 @@
 module MoySklad
   class BaseError < RuntimeError; end
   class BadApiResponse < BaseError; end
+  class EmptyCollection < BaseError; end
 end
