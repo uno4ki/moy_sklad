be0bbe9dd5ae1e2d252ead4efc7d3e9eb30bbc82
uno4ki <spam@junki.org>
2015-10-04 23:45:24 +0300

    Add "barcode" support to Goods Version bump 1.1.1

--- lib/moy_sklad/model/good.rb
+++ lib/moy_sklad/model/good.rb
@@ -1,5 +1,10 @@
 module MoySklad::Model
   class Good < MoySklad::Client::Base
+    def initialize(*args)
+      super(*args)
+      create_nested_collection(:barcode)
+    end
+
     def set_sale_price(type, value, currency)
       create_nested_resource(:salePrices)
 
@@ -14,6 +19,15 @@ module MoySklad::Model
     def get_sale_price(uuid)
       create_nested_resource(:salePrices)
       self.salePrices.find_object(:price, :priceTypeUuid, uuid)
+    end
+
+    def add_barcode(code, options)
+      barcode = create_and_load_resource("barcode", options)
+      if to_a(:barcode).empty?
+        self.barcode = [barcode]
+      else
+        self.barcode << barcode
+      end
     end
 
     private
